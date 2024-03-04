f876310234bdf96a5264902dbbaae5e584989d12
kallebysantos <kalleby_santos@hotmail.com>
2024-03-04 20:09:21 +0000

    📦 Wait timer

--- tmp/guessing_game.asm
+++ tmp/guessing_game.asm
@@ -1,10 +1,17 @@
 %DEFINE ORIGIN_SECTOR 0x7c00
 %DEFINE OK_RESULT 0xaa55
-; INTs
+
+; Display INTs
 %DEFINE DisplayStr 0x1301 ; AX INT - Display String and Update Cursor
 %DEFINE DisplayReset 0x03 ; AX INT - Set VGA Video Mode
 %DEFINE UpdateVideo 0x10 ; Set Video Mode
 %DEFINE TextGreen 0x02 ; BX INT
+
+; Timer INTs
+%DEFINE WaitTime 0x8600 ; AH INT - Wait
+%DEFINE SecondUnitCX 0x0f ; CX INT - 1 second
+%DEFINE SecondUnitDX 0x4240 ; DX INT - 1 second
+%DEFINE Time 0x15 ; Set Timer
 
 bits 16
 org ORIGIN_SECTOR 
@@ -18,25 +25,35 @@ main:
     init_stack:
         mov ss, ax
         mov sp, ORIGIN_SECTOR
+    
+    waiting:
+        mov ax, WaitTime
+        mov cx, SecondUnitCX
+        mov dx, SecondUnitDX
+        INT Time
+        
+        mov cx, 0 ; Limpa o CX
+        mov dx, 0 ; Limpa DX
 
     clear_screen:
-        mov ax, DisplayReset 
-        int UpdateVideo 
+       mov ax, DisplayReset 
+       INT UpdateVideo 
     
     print:
         mov ax, DisplayStr 
-        mov bx, 0x02
+        mov bx, TextGreen
         mov cx, padding - data
         mov bp, data
-        int UpdateVideo
+        INT UpdateVideo
 
     ; Parada de CPU
     halt:
         cli
         hlt
     
+
     data:
-        db "Hello World!"
+        db "Hello World!", 0
 
     ; Padding dos bytes restantes
     padding: 
